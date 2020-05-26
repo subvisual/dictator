d059a32fc5c549e394837a13a7ba8213a1b9f4ea
Miguel Palhas <mpalhas@gmail.com>
2020-05-26 20:55:57 +0100

    code review

--- mix.exs
+++ mix.exs
@@ -14,7 +14,12 @@ defmodule Dictator.MixProject do
       package: package(),
       deps: deps(),
       name: "Dictator",
-      source_url: "https://github.com/subvisual/dictator"
+      source_url: "https://github.com/subvisual/dictator",
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit],
+        ignore_warnings: ".dialyzer_ignore.exs",
+        flags: [:underspecs, :race_conditions]
+      ]
     ]
   end
 
@@ -35,7 +40,8 @@ defmodule Dictator.MixProject do
   defp deps(env) when env in [:dev, :test] do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false}
     ]
   end
 
