12e33a7ff724bf6209f3f2b6a8b248121e1a7f05
Miguel Palhas <mpalhas@gmail.com>
2020-05-28 18:38:02 +0100

    Removes default policy implementation
    Functions tagged with `defoverridable` are not used as a fallback if an
    upper level module re-implementes the behaviour. This means that the
    following policy
    
    ```
    defmodule Policy do
      use Dictator.Policy
    
      def can?(:very, :specific, :scenario), do: false
    end
    ```
    
    Would still fail to run for any other arguments, despite the original
    policy module defining a default implementation.
    
    This renders the default implementation pretty much useless, so
    I propose we remove it

--- lib/dictator/policy.ex
+++ lib/dictator/policy.ex
@@ -1,16 +1,9 @@
 defmodule Dictator.Policy do
   @callback can?(map() | struct(), atom(), map()) :: bool()
 
-  @optional_callbacks can?: 3
-
   defmacro __using__(_opts) do
     quote do
       @behaviour Dictator.Policy
-
-      @impl Dictator.Policy
-      def can?(_, _, _), do: false
-
-      defoverridable Dictator.Policy
     end
   end
 end
